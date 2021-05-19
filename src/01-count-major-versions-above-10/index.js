cbcb7854ff6b10f4891f417fcf0b87ffcaf40746
Beros <59673191+Panosmpero@users.noreply.github.com>
2021-05-19 12:44:15 +0100

    first test passed

--- src/01-count-major-versions-above-10/index.js
+++ src/01-count-major-versions-above-10/index.js
@@ -28,9 +28,28 @@ The results should have this structure:
  *  the number of packages that have a MAJOR semver version 
  *  greater than 10.x.x
  */
+const axios = require('axios');
+const ENDPOINT = 'http://ambush-api.inyourarea.co.uk/ambush/intercept';
+const postBody = {
+  url: 'https://api.npms.io/v2/search/suggestions?q=react',
+  method: 'GET',
+  return_payload: true,
+};
 
 module.exports = async function countMajorVersionsAbove10() {
-  // TODO
+  try {
+    const { data } = await axios.post(ENDPOINT, postBody);
+    const { content } = data;
 
-  return count
+    const result = content.filter(entry => {
+      const majorVersion = entry.package.version.split('.')[0];
+      return Number(majorVersion) > 10;
+    });
+
+    const count = result.length;
+
+    return count;
+  } catch (error) {
+    console.log(error);
+  }
 };
